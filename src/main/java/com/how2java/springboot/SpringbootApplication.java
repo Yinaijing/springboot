9e28a95a7da854ba5ec880838225a49806f64c12
yinaijing <jingjingyinai@163.com>
2018-12-11 11:30:33 +0800

    test3 test3

--- src/main/java/com/how2java/springboot/SpringbootApplication.java
+++ src/main/java/com/how2java/springboot/SpringbootApplication.java
@@ -9,5 +9,6 @@ public class SpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 		System.out.println("test2");
+		System.out.println("test3");
 	}
 }
